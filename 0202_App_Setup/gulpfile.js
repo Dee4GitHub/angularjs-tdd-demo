3890a9035f77117840526e8e13b428e2ae4cd1da
deepak4u <deepak.papanna@gmail.com>
2015-09-27 14:49:33 +0100

    Finished lesson and files related to this lesson is complete

---
+++ 0202_App_Setup/gulpfile.js
@@ -0,0 +1,20 @@
+/**
+ * Created by Deepak Papanna on 9/27/2015.
+ */
+var gulp = require('gulp');
+var browserSync = require('browser-sync');
+
+gulp.task('serve', function(){
+    browserSync.init({
+        notify: false,
+        port: 8080,
+        server: {
+            baseDir: ["app"],
+            routes: {
+                '/bower_components':'bower_components'
+            }
+        }
+    });
+    gulp.watch(['app/**/*.*'])
+        .on('change', browserSync.reload);
+});
