5a7dfb49be8c288fc0a3f9c620d00bc6cb55d450
deepak4u <deepak.papanna@gmail.com>
2015-10-15 01:21:27 +0100

    finished lesson 02 Selenium Configuration in Chapter 05

---
+++ 0502_ConfiguringSelenium/server.js
@@ -0,0 +1,20 @@
+/**
+ * Created by Deepak Papanna on 10/8/2015.
+ */
+var express = require('express');
+var cors = require('cors');
+var app = express();
+app.use(cors());
+var contacts = [
+    {
+        name: "Edard"
+    }, {
+        name: "Robert"
+    }
+];
+
+app.get('/contacts', function(req, res) {
+    res.status(200).json(contacts);
+});
+
+app.listen(9001);
---
+++ 0502_ConfiguringSelenium/test/e2e/app.spec.js
@@ -0,0 +1,8 @@
+/**
+ * Created by Deepak Papanna on 10/14/2015.
+ */
+describe('end to end address tests', function(){
+    it("should have contacts", function(done){
+        browser.get("http://localhost:8080");
+    })
+})
