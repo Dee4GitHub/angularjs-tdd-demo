5a2df4aeefab124d9ea2d0d1e88082fbf7cebbaf
deepak4u <deepak.papanna@gmail.com>
2015-10-18 02:00:31 +0100

    finished lesson 03 Updating the Contact Display in Chapter 06 Adding Finishing Touches

---
+++ 0603_UpdateContactDisp/app/main.js
@@ -0,0 +1,49 @@
+/**
+ * Created by Deepak Papanna on 9/27/2015.
+ */
+angular.module("AddressBook",[])
+    .service("contactService", function($http) {
+        this.contacts = [];
+        var contactService = this;
+        $http.get("http://localhost:9001/contacts")
+            .then(function(res) {
+                console.log(res);
+                while(res.data[0]){
+                    contactService.contacts.push(res.data.pop());
+                }
+            });
+        this.addContact = function(contact)
+        {
+            contactService.contacts.push(contact);
+        }
+    })
+    .controller("contactController", function(contactService, $scope){
+    $scope.contacts = contactService.contacts;
+    })
+
+    .filter("proper", function(){
+        return function(name){
+            var type = typeof name;
+
+            if(type !== 'number' && type !== 'string')
+                throw new Error();
+
+            return name.toString().split(" ").map(function(word){
+                return word[0].toUpperCase().concat(word.slice(1));
+            }).join(" ");
+        }
+    })
+    .directive("avatar", function(){
+        return{
+            restrict:"AE",
+            scope:{
+                name:"=",
+            },
+            template:"<span class='avatar'>{{name[0] | proper}}</span>"
+        }
+    })
+    .controller("AddContact", function(contactService, $scope){
+        $scope.AddContact = function(){
+            contactService.addContact($scope.contact);
+        }
+    })
